12c851dec9ecb73df860f408258005e9ebbd4acc
Fernando Fleury <fernandofleuryg@gmail.com>
2020-05-18 15:44:56 +0200

    feat(usepageviewtrigger): add usePageViewTrigger initial implementation

---
+++ src/hooks/usePageViewTrigger/usePageViewTrigger.spec.tsx
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2019, SumUp Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import { EVENTS } from '../../types';
+import TrackingRoot from '../../components/TrackingRoot';
+import usePageViewTrigger from './usePageViewTrigger';
+
+const DispatchButton = () => {
+  const dispatch = usePageViewTrigger();
+
+  return (
+    <button
+      data-testid="dispatch-btn"
+      onClick={() =>
+        dispatch({
+          component: 'button'
+        })
+      }
+    >
+      Dispatch button
+    </button>
+  );
+};
+
+describe('usePageViewTrigger', () => {
+  it('should provide a dispatch function that contains the pageView event', () => {
+    const dispatch = jest.fn();
+    const app = 'test-app-hook';
+    const btn = 'dispatch-btn';
+    const component = 'button';
+
+    const expected = {
+      app,
+      view: undefined,
+      zone: undefined,
+      event: EVENTS.pageView,
+      component,
+      id: undefined,
+      timestamp: expect.any(Number)
+    };
+
+    const { getByTestId } = render(
+      <TrackingRoot name={app} onDispatch={dispatch}>
+        <DispatchButton />
+      </TrackingRoot>
+    );
+
+    fireEvent.click(getByTestId(btn));
+
+    expect(dispatch).toHaveBeenCalledWith(expected);
+  });
+});
