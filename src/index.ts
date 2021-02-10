56baa216dc17af041f7db27148e91240a7c4c4a6
nicosommi <nico.sommi@sumup.com>
2021-02-10 17:18:19 +0100

    Export TrackingContext for testing purposes

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@
  */
 
 import TrackingRoot from './components/TrackingRoot';
+import TrackingContext from './components/TrackingContext';
 import TrackingView from './components/TrackingView';
 import TrackingElement from './components/TrackingElement';
 import useClickTrigger from './hooks/useClickTrigger';
@@ -24,6 +25,7 @@ import * as Types from './types';
 
 export {
   TrackingRoot,
+  TrackingContext,
   TrackingView,
   TrackingElement,
   useClickTrigger,
